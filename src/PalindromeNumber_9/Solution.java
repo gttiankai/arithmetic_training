256d4f093758c61db055b8060fa080a53265bb7d
Tian Kai <gttiankai@163.com>
2018-11-05 11:50:04 +0800

    Update: solve problem 9 （palindrome number 回文数字）

---
+++ src/PalindromeNumber_9/Solution.java
@@ -0,0 +1,74 @@
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Example 1:
+ *
+ * Input: 121
+ * Output: true
+ * Example 2:
+ *
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ *
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * Follow up:
+ *
+ * Coud you solve it without converting the integer to a string?
+ * */
+package PalindromeNumber_9;
+
+public class Solution {
+    /**
+     * convert the integer to string
+     *
+     * */
+    public static boolean isPalindrome(int x) {
+        char[] chars = Integer.toString(x).toCharArray();
+        int length = chars.length;
+        int middle = chars.length/2;
+        if (chars.length%2 == 0) {
+            // 偶数
+            for (int i = 0; i < middle; i++) {
+                if(chars[i] != chars[length-1-i]){
+                    return false;
+                }
+            }
+        } else {
+            // 奇数
+            for (int i = 0; i <= middle; i++) {
+                if(chars[i] != chars[length-1-i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    /**
+     * do not convert int to string
+     * */
+    public static boolean isPalindrome2(int x) {
+        if (x < 0 || (x!=0 && x%10 == 0)) {
+            return false;
+        }
+        int rev = 0;
+
+        /**
+         * 将值进行翻转，然后进行比较
+         * */
+        while (x > rev) {
+            rev = rev *10 + x%10;
+            x = x/10;
+        }
+        return (x == rev || x == rev/10);
+    }
+    public static void main(String[] arguments) {
+        int x = 123456;
+        String message ;
+        message =  isPalindrome2(x) ? " is palindrome" : " is not palindrome";
+        System.out.println("the " + x + message);
+    }
+}
