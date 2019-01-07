acd1b7e93e25e7b805c86aab6ea7a08391b547d2
Tian Kai <gttiankai@163.com>
2019-01-07 16:57:35 +0800

    Update: 学习了 jump game的所有的算法, 收获很大

--- src/jump_game_55/Solution.java
+++ src/jump_game_55/Solution.java
@@ -73,16 +73,25 @@ public class Solution {
         if (nums[index] == 0) {
             return false;
         }
+        // 未优化版本
+//        for(int i = index + 1; i <= nums[index] + index ; i++) {
+//            if (true == backtrace(nums, i)) {
+//                return true;
+//            }
+//        }
+        // 这个版本是优化过得.是从右到左进行,
         for (int i = nums[index]; i > 0; i--) {
             if (true == backtrace(nums, index + i)) {
                 return true;
             }
         }
+
+
         return false;
     }
 
     /**
-     * 最简单的答案,但是没有弄清楚
+     * 最简单的答案,但是没有弄清楚,
      * */
 
     public static boolean canJumpUnknown(int[] nums) {
@@ -96,45 +105,117 @@ public class Solution {
         return true;
     }
 
+    /**
+     * dp方法,dp的方法结合了上面的回溯的方法,但是在回溯法的基础上进行了优化
+     * 优化的地方: 引入了dp[i], 表示i位置能否jump到最后一个位置.
+     * dp[i]的初始化为unknown, 然后使用动态规划的方案进行处理.
+     *
+     * WARNING: 即使使用了这个动态规划的算法,发现运行时间还是超时了, 需要进一步提高运行效率
+     *
+     * */
     enum Index {
         GOOD, BAD,UNKNOWN
     }
-    Index[] memo;
+    public static Index[] memo;
 
-    public boolean canJumpFromPosition(int position, int[] nums) {
-        if (memo[position] != Index.UNKNOWN) {
-            return memo[position] == Index.GOOD ? true : false;
-        }
-
-        int furthestJump = Math.min(position + nums[position], nums.length - 1);
-        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-            if (canJumpFromPosition(nextPosition, nums)) {
-                memo[position] = Index.GOOD;
-                return true;
-            }
-        }
-
-        memo[position] = Index.BAD;
-        return false;
-    }
-
-    public boolean canJump(int[] nums) {
+    public static boolean canJumpFailedThirty(int[] nums) {
         memo = new Index[nums.length];
         // initial the memory map
         for (int i = 0; i < memo.length; i++) {
             memo[i] = Index.UNKNOWN;
         }
+        // 最后一个节点,本身就已经在最后了,所以肯定能够到达的.
         memo[memo.length - 1] = Index.GOOD;
         return canJumpFromPosition(0, nums);
+    }
+
+    public static boolean canJumpFromPosition(int position, int[] nums) {
+        if (memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD ? true : false;
+        }
+
+        /**
+         * 这个版本是优化前的版本,
+         * */
+//        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+//        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+//            if (canJumpFromPosition(nextPosition, nums)) {
+//                memo[position] = Index.GOOD;
+//                return true;
+//            }
+//        }
+        // 当有了memo之后就必须添加这个,因为如果nextPosition会超出memo的大小,造成内存数组越界的问题
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = furthestJump; nextPosition > position; nextPosition--) {
+            // 其实这个里面有一个优化的地方,就是如何消除递归
+            if (canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+        memo[position] = Index.BAD;
+        return false;
+    }
+
+
+    /**
+     * 接下来要进行进一步的进行效率的优化.
+     * 优化的具体的思路就是消除递归,这样才能满足需求.
+     * 消除递归的通常的方式: The recursion is usually eliminated by trying to reverse the order of the steps
+     *  from the top-down approach.
+     *
+     *
+     * */
+    public  Index[] dp;
+    public  boolean canJumpDP(int[] nums) {
+        if (null == nums || nums.length == 0) {
+            return false;
+        }
+        dp = new Index[nums.length];
+        for (Index item:
+             dp) {
+            item = Index.UNKNOWN;
+        }
+        dp[nums.length-1] = Index.GOOD;
+        for (int i = nums.length - 2; i >= 0; i--){
+            int furthestJump = Math.min(nums[i] + i, nums.length-1);
+            // 其实这个方案的思想和上一个递归的方案是一致的,但是由于消除了递归,所以速度上会快.能够通过测试.
+            for (int j = i + 1; j <= furthestJump; j ++) {
+                if (dp[j] == Index.GOOD) {
+                    dp[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+        return dp[0] == Index.GOOD;
+    }
+
+    /**
+     * 通过看答案居然能够使用贪心算法, 太牛了
+     *
+     * */
+    public static boolean canJump(int[] nums) {
+        if (nums == null) {
+            return false;
+        }
+        int lastPosition = nums.length - 1;
+        for (int i = lastPosition ; i >= 0; i--) {
+            //
+            if (i + nums[i] >= lastPosition) {
+                lastPosition = i;
+            }
+        }
+        return lastPosition == 0;
     }
 
 
     public static void main(String[] arguments) {
         int[] nums = {3, 2, 1, 0, 4}; // false
         int[] nums1 = {2, 3, 1, 1, 4};// true
-        int[] nums2 = {0, 1};
-        int[] nums3 = {2, 0, 1, 0, 1};
-        int[] nums4 = {0};
-        System.out.printf("" + canJump(nums));
+        int[] nums2 = {0, 1}; // false
+        int[] nums3 = {2, 0, 1, 0, 1}; // false
+        int[] nums4 = {0}; // true
+        int[] nums5 = {2,0};
+        System.out.printf("" + canJump(nums5));
     }
 }
