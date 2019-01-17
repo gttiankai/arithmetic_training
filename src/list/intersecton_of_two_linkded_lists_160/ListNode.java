86d176265ddc69ff2c19d7bdbeebbb9e6ee470b1
Tian Kai <gttiankai@163.com>
2019-01-17 22:30:37 +0800

    Update:    1. solve 160: intersection if two linked lists. Time: O(n), Space O(1)

---
+++ src/list/intersecton_of_two_linkded_lists_160/ListNode.java
@@ -0,0 +1,11 @@
+package list.intersecton_of_two_linkded_lists_160;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
