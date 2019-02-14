c751ccc26ed60cd1ccbaa43b139a92b19eb01aa3
Tian Kai <gttiankai@163.com>
2019-02-14 22:56:12 +0800

    Update:    1. 581 学习到了一种新的方式,后面需要总结

--- src/array/shortest_unsorted_continuous_subarray_581/Solution.java
+++ src/array/shortest_unsorted_continuous_subarray_581/Solution.java
@@ -19,6 +19,8 @@
 
 package array.shortest_unsorted_continuous_subarray_581;
 
+import java.util.Arrays;
+
 public class Solution {
     /**
      * two pointer 的方式是不对的, 不能完成这个任务
@@ -64,12 +66,107 @@ public class Solution {
         }
     }
 
+    /**
+     * 根据提示可以知道可以通过排序的方式进行解决
+     * TODO: 是不是大部分的 array 的题目都可以通过排序的方式进行解决?
+     * */
     public int findUnsortedSubarray(int[] nums) {
-        return 0;
+        int[] sortedNums = nums.clone();
+        Arrays.sort(sortedNums);
+        int i = 0;
+        int j = nums.length-1;
+        boolean left = true;
+        boolean right = true;
+
+        /*
+        * 这一部分是根据我的逻辑进行编写的
+        * */
+        while (i < j) {
+            if (left) {
+                if (nums[i] != sortedNums[i]) {
+                    left = false;
+                } else {
+                    i++;
+                }
+            }
+            if (right) {
+                if (nums[j] != sortedNums[j]) {
+                    right = false;
+                } else {
+                    j--;
+                }
+            }
+
+            if (!left && !right) {
+                break;
+            }
+        }
+        if (i == j) {
+            return 0;
+        } else {
+            return (j - i + 1);
+        }
+
     }
+
+
+    public int findUnsortedSubarray2(int[] nums) {
+        int[] sortedNums = nums.clone();
+        Arrays.sort(sortedNums);
+        int start = sortedNums.length, end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 这个循环比较巧妙解决了我上面的代码过多的问题
+            // 每个元素都会参与循环,但是是通过比较最大值和最小值的方式的
+            if (sortedNums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return end - start >= 0? end - start +1:0;
+
+    }
+
+    public int findUnsortedSubarrayStack(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] > nums[i]) {
+                flag = true;
+            }
+            if (flag) {
+                min = Math.min(min, nums[i]);
+            }
+        }
+        flag = false;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1]) {
+                flag = true;
+            }
+            if (flag){
+                max = Math.max(max, nums[i]);
+            }
+        }
+        int l, r;
+        for (l = 0; l < nums.length; l++) {
+            if (min < nums[l]) {
+                break;
+            }
+        }
+        for (r = nums.length - 1; r >= 0; r--) {
+            if (max > nums[r]) {
+                break;
+            }
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+
+
+
+
     public static void main(String[] arguments) {
         Solution solution = new Solution();
         int[] nums = {1, 2, 5, 3, 4};
-        System.out.printf(""+solution.findUnsortedSubarray(nums));
+        System.out.printf(""+solution.findUnsortedSubarrayStack(nums));
     }
 }
