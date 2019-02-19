eb8a5a23ffe4ab4975fcced84181ef8dc1725726
Tian Kai <gttiankai@163.com>
2019-02-19 14:40:23 +0800

    Update: 1. 207题目还没有解决， 只是看到的答案，但是还没有解决这个问题

--- src/search/course_schedule_207/Solution.java
+++ src/search/course_schedule_207/Solution.java
@@ -33,8 +33,79 @@
 
 package search.course_schedule_207;
 
+import java.util.*;
+
 public class Solution {
+    //
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
+        ArrayList[] graph = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList();
+        }
+
+        boolean[] visited = new boolean[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        // 从每一节课的角度进行遍历
+        for (int i = 0; i < numCourses; i++) {
+            if (!DFS(graph, visited, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean DFS(ArrayList[] graph, boolean[] visited, int course) {
+        if (visited[course]){
+            // 已经成了环了，所以直接返回
+            return false;
+        }else {
+            visited[course] = true;
+        }
+        for (int i = 0; i < graph[course].size(); i++) {
+            if (!DFS(graph, visited, (int)graph[course].get(i))){
+                return false;
+            }
+        }
+        visited[course] = false;
+        return true;
+    }
+
+
+    /**
+     * 讲真的，这个算法有点看不懂，所以后面需要加强
+     * */
+    public boolean canFinishF(int numCourses, int[][] prerequisites) {
+        int[][] matrix = new int[numCourses][numCourses];
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            int ready = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            if (matrix[pre][ready] == 0) {
+                //duplicate case
+                indegree[ready]++;
+            }
+            matrix[pre][ready] = 1;
+        }
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            for (int i = 0; i < numCourses; i++) {
+                if (matrix[course][i] != 0) {
+                    if (--indegree[i] == 0) {
+                        queue.offer(i);
+                    }
+                }
+            }
+        }
+        return count == numCourses;
     }
 }
