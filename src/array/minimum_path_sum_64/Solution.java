9fac53e1d6068d8dc21199519c8f5227648d3c7b
Tian Kai <gttiankai@163.com>
2019-03-19 16:43:30 +0800

    Update: 1. 解决了 64 minimum path sum问题，从答案中来的。自己的思路走入死胡同

---
+++ src/array/minimum_path_sum_64/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which
+ *
+ * minimizes the sum of all numbers along its path.
+ *
+ * Note: You can only move either down or right at any point in time.
+ *
+ * Example:
+ *
+ * Input:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ *
+ * */
+
+package array.minimum_path_sum_64;
+
+public class Solution {
+    /**
+     * 刚一开始没有做出来，还是因为没有考虑清楚题目中的限制，导致自己的思路走入了死胡同
+     *
+     * */
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int j = 1; j < m; j++) {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
