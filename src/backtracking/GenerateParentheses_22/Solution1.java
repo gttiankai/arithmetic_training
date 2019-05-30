f0532dc11cdb3fb677e10bc9c66f8e26e67d899b
Tian Kai <gttiankai@163.com>
2019-05-31 00:59:39 +0800

    Update:     1. 22题目,还有想明白怎么解决.说明对回溯法的理解还是有问题.需要继续加强.

---
+++ src/backtracking/GenerateParentheses_22/Solution1.java
@@ -0,0 +1,81 @@
+package backtracking.GenerateParentheses_22;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution1 {
+    /**
+     * 这个题目同样需要是用回溯法
+     */
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        backTrace(ans, new StringBuilder('('),0, n);
+        return ans;
+    }
+
+    private void backTrace(ArrayList<String> ans, StringBuilder tmpString,  int index, int slots) {
+        if (index == slots *2) {
+            ans.add(tmpString.toString());
+            return ;
+        }
+
+        if (isValid(tmpString.append('(').toString(), slots)) {
+            backTrace(ans, tmpString, index+1, slots);
+            tmpString.deleteCharAt(tmpString.length()-1);
+        }
+
+        if (isValid(tmpString.append(')').toString(), slots)){
+            backTrace(ans, tmpString, index+1, slots);
+            tmpString.deleteCharAt(tmpString.length()-1);
+        }
+
+    }
+    private boolean isValid(String string, int slots) {
+        if (string == null || string.length() == 0) {
+            return true;
+        }
+        int left = 0;
+        int right = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < string.length();i++) {
+            if (string.charAt(i) == '(') {
+                left++;
+                if (left > slots) {
+                    return false;
+                }
+            }
+            if (string.charAt(i) == ')') {
+                right++;
+                if (right > slots) {
+                    return false;
+                }
+            }
+            if (stack.size() == 0) {
+                if (string.charAt(i) == ')') {
+                    return false;
+                }
+                stack.push(string.charAt(i));
+            } else {
+                if (stack.peek() == '(' && string.charAt(i) == ')') {
+                    stack.pop();
+                } else {
+                    stack.push(string.charAt(i));
+                }
+            }
+            if (stack.size() > slots) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] arguments) {
+        int n = 3;
+        Solution1 solution = new Solution1();
+        List<String> ans = solution.generateParenthesis(n);
+        System.out.println(ans);
+    }
+}
