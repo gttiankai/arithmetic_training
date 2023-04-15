206087bd9f2d0a441cff0f4529b87623e7e482c6
lucasktian <lucasktian@tencent.com>
2023-04-15 23:57:18 +0800

    [UPD]1. up load 5(dp implement);

--- cpp_implement/5.cc
+++ cpp_implement/5.cc
@@ -61,13 +61,69 @@ class SolutionSlow {
  * */
 class Solution {
  public:
-  std::string longestPalindrome(std::string s) {}
+  std::string longestPalindrome(std::string s) {
+    int size = s.size();
+    if (size < 2) {
+      return s;
+    }
+    std::string res;
+    if (size == 2) {
+      if (s[0] == s[1]) {
+        return s;
+      } else {
+        return s.substr(0, 1);
+      }
+    }
+    // 记录回文字符串的最大长度,用于最后的判断
+    int max_len = 1;
+    // 记录最长回文子字符串起始位置,和 max_len 配置就可以获取到最长回文子字符串
+    int start = 0;
+    std::vector<std::vector<bool>> dp(size, std::vector<bool>(size, false));
+    // initial dp
+    for (int i = 0; i < size; ++i) {
+      dp[i][i] = true;
+    }
+    // 子字符串的长度从 2 到 size
+    for (int sub_str_len = 2; sub_str_len <= size; ++sub_str_len) {
+      for (int i = 0; i < size; ++i) {
+        // sub_str_len = j - i + 1
+        int j = sub_str_len + i - 1;
+        // WARNING: j 是有可能等于 size 的, 尤其是 sub_str_len = size
+        if (j >= size) {
+          break;
+        }
+        // dp[i][j] = dp[i + 1][j - 1] && (s[i] == s[j])
+        if (s[i] != s[j]) {
+          dp[i][j] = false;
+        } else {
+          // s[i] == s[j]
+          if (j - i < 3) {
+            dp[i][j] = true;
+          } else {
+            dp[i][j] = dp[i + 1][j - 1];
+          }
+        }
+        if (dp[i][j] && max_len < j - i + 1) {
+          max_len = j - i + 1;
+          start   = i;
+        }
+      }
+    }
+    return s.substr(start, max_len);
+  }
 };
-
 
 int main(int argc, char* argv[]) {
   Solution solution;
   // std::string message = "babad";
-  std::string message = "abcd";
+  // std::string message = "ab";
+  std::string message =
+      "uzhynqvopdbnkvuxizirzjsslptlhmvyfqhqvqffmqldkrrdwapbdcxqbchvxqixmvbbtalrgzvkobyxlkonkfknabjwzeoazankqzuhexhcwkbv"
+      "wtioubrcujqqeqoedhploqklhgeilwwnndbnzeadzefkcvaxdhgnmocadvvzjocoweyoidhleuwhmavpdiizbwkukpstyorlwwyiwwyyyzqqgipz"
+      "zlxsgcdjscdfvrrrqmllbdjkkuisxeqaprfblvjuixajhucmcvffmevaztvadrujbthlnsdrxzvbldwxbazxmilpkbccigkihcgjbtpvignmrgzd"
+      "qnufaacxtihfgwrllrwgfhitxcaafunqdzgrmngivptbjgchikgiccbkplimxzabxwdlbvzxrdsnlhtbjurdavtzavemffvcmcuhjaxiujvlbfrp"
+      "aqexsiukkjdbllmqrrrvfdcsjdcgsxlzzpigqqzyyywwiywwlroytspkukwbziidpvamhwuelhdioyewocojzvvdacomnghdxavckfezdaeznbdn"
+      "nwwlieghlkqolphdeoqeqqjucrbuoitwvbkwchxehuzqknazaoezwjbankfknoklxybokvzgrlatbbvmxiqxvhcbqxcdbpawdrrkdlqmffqvqhqf"
+      "yvmhltplssjzrizixuvknbdpovqnyhzu";
   std::cout << solution.longestPalindrome(message) << std::endl;
 }
