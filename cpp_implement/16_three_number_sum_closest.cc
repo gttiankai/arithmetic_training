7bbdaa83eb463c47ead10c9e5f7e0857b37e3d10
lucasktian <lucasktian@tencent.com>
2023-05-16 21:21:43 +0800

    [BUG]1. upload 16 problem

---
+++ cpp_implement/16_three_number_sum_closest.cc
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <string>
+#include <vector>
+
+/**
+1. 暴力枚举解法:
+
+ */
+
+class SolutionSlow {
+ public:
+  int threeSumClosest(std::vector<int>& nums, int target) {
+    int N = nums.size();
+    int diff = INT_MAX;
+    int ans;
+    for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+        for (int k = j + 1; k < N; ++k) {
+          int sum = nums[i] + nums[j] + nums[k];
+          if (sum == target) {
+            return sum;
+          } else {
+            if (diff > std::abs(sum - target)) {
+              diff = std::abs(sum - target);
+              ans = sum;
+            }
+          }
+        }
+      }
+    }
+    return ans;
+  }
+};
+
+class Solution {
+ public:
+  int threeSumClosest(std::vector<int>& nums, int target) {
+    int ans = 0;
+    int N = nums.size();
+    // sort nums by ascending order
+    std::sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+      }
+    }
+
+    return ans = 0;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  std::vector<int> nums = {-1, 2, 1, -4};
+  int target = 1;
+  SolutionSlow solution;
+  std::cout << solution.threeSumClosest(nums, target) << std::endl;
+  return 0;
+}
