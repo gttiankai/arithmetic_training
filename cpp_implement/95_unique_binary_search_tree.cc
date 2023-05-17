ea1e51dd58337530f550c294d5dffab8a835535e
lucasktian <lucasktian@tencent.com>
2023-05-17 21:16:36 +0800

    [BUG]1. hit 95 problem;

--- cpp_implement/95_unique_binary_search_tree.cc
+++ cpp_implement/95_unique_binary_search_tree.cc
@@ -12,16 +12,41 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 #include <iostream>
-#include <string>
 #include <vector>
 #include "tree_node.h"
 
 class Solution {
  public:
   std::vector<TreeNode*> generateTrees(int n) {
+    return CreateBST(1, n);
+  }
+
+ private:
+  std::vector<TreeNode*> CreateBST(int start, int end) {
+    if (start > end) {
+      return {nullptr};
+    }
+    std::vector<TreeNode*> all_trees;
+    for (int i = start; i <= end; ++i) {
+      std::vector<TreeNode*> left_trees  = CreateBST(start, i - 1);
+      std::vector<TreeNode*> right_trees = CreateBST(i + 1, end);
+      for (const auto& left : left_trees) {
+        for (const auto& right : right_trees) {
+          TreeNode* currTree = new TreeNode(i);
+          currTree->left     = left;
+          currTree->right    = right;
+          all_trees.emplace_back(currTree);
+        }
+      }
+    }
+    return all_trees;
   }
 };
 
 int main(int argc, char* argv[]) {
+  Solution solution;
+  int n    = 3;
+  auto ans = solution.generateTrees(n);
+  std::cout << n << " generate bst: " << ans.size() << std::endl;
   return 0;
 }
