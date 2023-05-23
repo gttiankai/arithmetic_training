6d9dc172189b008e8e9fc92adfaf66e86dc215b7
lucasktian <lucasktian@tencent.com>
2023-05-24 01:00:19 +0800

    [UPD]1. upload 62 problem;

---
+++ cpp_implement/62.unique-paths.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=cpp
+ *
+ * [62] Unique Paths
+ *
+ * https://leetcode.cn/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (67.73%)
+ * Likes:    1771
+ * Dislikes: 0
+ * Total Accepted:    621.2K
+ * Total Submissions: 917.2K
+ * Testcase Example:  '3\n7'
+ *
+ * There is a robot on an m x n grid. The robot is initially located at the
+ * top-left corner (i.e., grid[0][0]). The robot tries to move to the
+ * bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move
+ * either down or right at any point in time.
+ * 
+ * Given the two integers m and n, return the number of possible unique paths
+ * that the robot can take to reach the bottom-right corner.
+ * 
+ * The test cases are generated so that the answer will be less than or equal
+ * to 2 * 10^9.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: m = 3, n = 7
+ * Output: 28
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation: From the top-left corner, there are a total of 3 ways to reach
+ * the bottom-right corner:
+ * 1. Right -> Down -> Down
+ * 2. Down -> Down -> Right
+ * 3. Down -> Right -> Down
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= m, n <= 100
+ * 
+ * 
+ */
+#include <iostream>
+// @lc code=start
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+
+    }
+};
+// @lc code=end
+int main(int argc, const char** argv) {
+    int m = 3;
+    int n = 2;
+    Solution solution;
+    std::cout << solution.uniquePaths(m , n) << std::endl;
+    return 0;
+}
