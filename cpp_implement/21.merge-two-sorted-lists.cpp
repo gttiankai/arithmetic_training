9950777785659bf02329b7a4132bebd1b63c83e1
lucasktian <lucasktian@tencent.com>
2023-05-24 23:23:23 +0800

    [UPD]1. hit 21 prolem;2. TODO: recursion implement 3. TODO: iteration implement;

---
+++ cpp_implement/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,126 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.cn/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (66.22%)
+ * Likes:    3116
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: list1 = [], list2 = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ *
+ *
+ */
+#include <cmath>
+#include <iostream>
+#include <locale>
+#include <ostream>
+#include <string>
+#include <vector>
+#include "list_node.h"
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* root = nullptr;
+    if (list1 == nullptr) {
+      return list2;
+    } else if (list2 == nullptr) {
+      return list1;
+    }
+    // list1 != nullptr && list2 != nullptr
+    if (list1->val <= list2->val) {
+      root  = list1;
+      list1 = list1->next;
+    } else {
+      root  = list2;
+      list2 = list2->next;
+    }
+    ListNode* guard = root;
+    while (list1 != nullptr || list2 != nullptr) {
+      if (list1 == nullptr) {
+        guard->next = list2;
+        break ;
+      } else if (list2 == nullptr) {
+        guard->next = list1;
+        break ;
+      } else {
+        if (list1->val <= list2->val) {
+          guard->next = list1;
+          guard       = guard->next;
+          list1       = list1->next;
+        } else {
+          guard->next = list2;
+          guard       = guard->next;
+          list2       = list2->next;
+        }
+      }
+    }
+    return root;
+  }
+};
+// TODO: 实现递归版本
+// TODO: 实现迭代的版本
+// @lc code=end
+
+int main(int argc, char* argv[]) {
+  ListNode* list1 = StringToListNode("[1, 2, 4]");
+  ListNode* list2 = StringToListNode("[1, 3, 4]");
+  Solution solution;
+  auto ans = solution.mergeTwoLists(list1, list2);
+  std::cout << ListNodeToString(ans) << std::endl;
+
+  return 0;
+}
---
+++ cpp_implement/list_node.h
@@ -0,0 +1,79 @@
+// Tencent is pleased to support the open source community by making TNN available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#ifndef ARITHMETIC_TRAINING_LIST_NODE_H
+#define ARITHMETIC_TRAINING_LIST_NODE_H
+#include <sstream>
+#include <string>
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+void trimLeftTrailingSpaces(string& input) {
+  input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) { return !isspace(ch); }));
+}
+
+void trimRightTrailingSpaces(string& input) {
+  input.erase(find_if(input.rbegin(), input.rend(), [](int ch) { return !isspace(ch); }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+  vector<int> output;
+  trimLeftTrailingSpaces(input);
+  trimRightTrailingSpaces(input);
+  input = input.substr(1, input.length() - 2);
+  stringstream ss;
+  ss.str(input);
+  string item;
+  char delim = ',';
+  while (getline(ss, item, delim)) {
+    output.push_back(stoi(item));
+  }
+  return output;
+}
+
+ListNode* StringToListNode(string input) {
+  // Generate list from the input
+  vector<int> list = stringToIntegerVector(input);
+
+  // Now convert that list into linked list
+  ListNode* dummyRoot = new ListNode(0);
+  ListNode* ptr       = dummyRoot;
+  for (int item : list) {
+    ptr->next = new ListNode(item);
+    ptr       = ptr->next;
+  }
+  ptr = dummyRoot->next;
+  delete dummyRoot;
+  return ptr;
+}
+
+string ListNodeToString(ListNode* node) {
+  if (node == nullptr) {
+    return "[]";
+  }
+
+  string result;
+  while (node) {
+    result += to_string(node->val) + ", ";
+    node = node->next;
+  }
+  return "[" + result.substr(0, result.length() - 2) + "]";
+}
+#endif  // ARITHMETIC_TRAINING_LIST_NODE_H
