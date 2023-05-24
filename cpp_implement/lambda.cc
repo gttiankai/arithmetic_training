1fb71ba6585a228297495254b1daad7808bf1b4e
lucasktian <lucasktian@tencent.com>
2023-05-24 21:12:33 +0800

    [UPD]1. upload lamada example;

--- cpp_implement/lambda.cc
+++ cpp_implement/lambda.cc
@@ -1,3 +1,23 @@
-//
-// Created by 田凯 on 2022/5/31.
-//
+#include <iostream>
+#include <vector>
+
+bool cmp(int a, int b) {
+  return a < b;
+}
+
+int main(int argc, char* argv[]) {
+  std::vector<int> array = {3, 2, 5, 7, 3, 2};
+  auto array_bp(array);
+
+  std::sort(array.begin(), array.end(), cmp);
+  for (const auto& iter : array) {
+    std::cout << iter << " ";
+  }
+  std::cout << std::endl;
+
+  std::sort(array_bp.begin(), array_bp.end(), [](int a, int b) { return a > b; });
+  for (const auto& iter : array_bp) {
+    std::cout << iter << " ";
+  }
+  std::cout << std::endl;
+}
