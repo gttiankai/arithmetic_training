0a00c49e28f2a4a5b2332cab5ddb7fe038e9f239
lucasktian <lucasktian@tencent.com>
2023-05-24 14:26:37 +0800

    [UPD]1. hit 62 problem;

--- cpp_implement/62.unique-paths.cpp
+++ cpp_implement/62.unique-paths.cpp
@@ -17,24 +17,24 @@
  * top-left corner (i.e., grid[0][0]). The robot tries to move to the
  * bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move
  * either down or right at any point in time.
- * 
+ *
  * Given the two integers m and n, return the number of possible unique paths
  * that the robot can take to reach the bottom-right corner.
- * 
+ *
  * The test cases are generated so that the answer will be less than or equal
  * to 2 * 10^9.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: m = 3, n = 7
  * Output: 28
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: m = 3, n = 2
  * Output: 3
  * Explanation: From the top-left corner, there are a total of 3 ways to reach
@@ -42,29 +42,41 @@
  * 1. Right -> Down -> Down
  * 2. Down -> Down -> Right
  * 3. Down -> Right -> Down
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= m, n <= 100
- * 
- * 
+ *
+ *
  */
 #include <iostream>
 // @lc code=start
 class Solution {
-public:
-    int uniquePaths(int m, int n) {
-
+ public:
+  int uniquePaths(int m, int n) {
+    int dp[m][n];
+    for (int i = 0; i < n; ++i) {
+      dp[0][i] = 1;
     }
+    for (int i = 0; i < m; ++i) {
+      dp[i][0] = 1;
+    }
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
 };
 // @lc code=end
 int main(int argc, const char** argv) {
-    int m = 3;
-    int n = 2;
-    Solution solution;
-    std::cout << solution.uniquePaths(m , n) << std::endl;
-    return 0;
+  int m = 3;
+  int n = 2;
+  Solution solution;
+  std::cout << solution.uniquePaths(m, n) << std::endl;
+  return 0;
 }
