48d83cf439bb062deee0b96601c765ecf4073ac0
lucasktian <lucasktian@tencent.com>
2023-05-24 21:11:38 +0800

    [UPD]1. hit 91 problem;

--- cpp_implement/91.decode-ways.cpp
+++ cpp_implement/91.decode-ways.cpp
@@ -54,8 +54,7 @@
  *
  * Input: s = "226"
  * Output: 3
- * Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2
- * 2 6).
+ * Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
  *
  *
  * Example 3:
@@ -63,8 +62,7 @@
  *
  * Input: s = "06"
  * Output: 0
- * Explanation: "06" cannot be mapped to "F" because of the leading zero ("6"
- * is different from "06").
+ * Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
  *
  *
  *
@@ -78,22 +76,76 @@
  */
 #include <iostream>
 #include <string>
+#include <vector>
 
 // @lc code=start
+/**
+ * 这个题目很好,
+ *  1. dynamic programing
+ *     dp(0) = 0
+ *      IsValid(s[i]) ?
+ *          YES: dp(i) += dp(i-1)
+ *          NO:  dp(i) += 0
+ *      IsValid(s[i-1],s[i]) ?
+ *          YES: dp(i) += dp(i-2);
+ *          NO:  dp(i) += 0
+ * */
 class Solution {
  public:
   int numDecodings(std::string s) {
-    int ans = 0;
-    return ans;
+    if (s.empty()) {
+      return 0;
+    }
+    int N = s.length();
+    std::vector<int> dp(N, 0);
+    dp[0] = IsValid(s[0]) ? 1 : 0;
+    if (dp[0] == 0) {
+      return 0;
+    }
+    if (N > 1) {
+      if (IsValid(s[1])) {
+        dp[1] += 1;
+      }
+      if (IsValid(s.substr(0, 2))) {
+        dp[1] += 1;
+      }
+    }
+    for (int i = 2; i < N; ++i) {
+      if (IsValid(s[i])) {
+        dp[i] += dp[i - 1];
+      }
+      if (IsValid(s.substr(i - 1, 2))) {
+        dp[i] += dp[i - 2];
+      }
+      if (dp[i] == 0) {
+        return 0;
+      }
+    }
+    return dp[N - 1];
+  }
+
+ private:
+  bool IsValid(char c) {
+    return ('1' <= c && c <= '9');
+  }
+  bool IsValid(std::string sub_str) {
+    if (sub_str.size() != 2 || sub_str[0] == '0') {
+      return false;
+    }
+    int num = atoi(sub_str.c_str());
+    return (10 <= num && num <= 26);
   }
 };
 // @lc code=end
 
-int main(int argc, char *argv[]) {
-  std::string input   = "226";
-  std::string input_1 = "06";
+int main(int argc, char* argv[]) {
   Solution solution;
-  std::cout << input << ": " << solution.numDecodings(input) << std::endl;
-  std::cout << input_1 << ": " << solution.numDecodings(input_1) << std::endl;
+  std::vector<std::string> message = {"226", "06",
+                                      "0522"
+                                      "200",
+                                      "2012", "201"};
+  for (const auto& item : message) {
+    std::cout << item << " num decode :" << solution.numDecodings(item) << std::endl;
+  }
   return 0;
 }
