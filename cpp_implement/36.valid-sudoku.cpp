9318ef87dd4ab9c0ed6254dc9610cad8f56baabb
lucasktian <lucasktian@tencent.com>
2023-06-09 00:08:46 +0800

    [UPD]1. hit 36 problem;2. 但是 36 的性能不是最优的。

--- cpp_implement/36.valid-sudoku.cpp
+++ cpp_implement/36.valid-sudoku.cpp
@@ -11,11 +11,12 @@
  * Dislikes: 0
  * Total Accepted:    375.6K
  * Total Submissions: 595.7K
- * Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
+ * Testcase Example:
+ '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
  *
  * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be
  * validated according to the following rules:
- * 
+ *
  * Each row must contain the digits 1-9 without repetition.
  * Each column must contain the digits 1-9 without repetition.
  * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9
@@ -30,7 +31,7 @@
  *
  * Example 1:
  *
- * Input: board = 
+ * Input: board =
  * [["5","3",".",".","7",".",".",".","."]
  * ,["6",".",".","1","9","5",".",".","."]
  * ,[".","9","8",".",".",".",".","6","."]
@@ -41,9 +42,9 @@
  * ,[".",".",".","4","1","9",".",".","5"]
  * ,[".",".",".",".","8",".",".","7","9"]]
  * Output: true
- * 
+ *
  * Example 2:
- * 
+ *
  * Input: board =
  * [["8","3",".",".","7",".",".",".","."]
  * ,["6",".",".","1","9","5",".",".","."]
@@ -64,8 +65,8 @@
  * board.length == 9
  * board[i].length == 9
  * board[i][j] is a digit 1-9 or '.'.
- * 
- * 
+ *
+ *
  */
 
 #include <iostream>
@@ -74,14 +75,73 @@
 
 // @lc code=start
 class Solution {
-public:
-    bool isValidSudoku(std::vector<std::vector<char>>& board) {
-
+ public:
+  bool isValidSudoku(std::vector<std::vector<char>>& board) {
+    std::vector<std::vector<bool>> row_flag(9, std::vector<bool>(9, false));
+    std::vector<std::vector<bool>> col_flag(9, std::vector<bool>(9, false));
+    std::vector<std::vector<bool>> square_flag(9, std::vector<bool>(9, false));
+    for (int i = 0; i < 9; ++i) {
+      for (int j = 0; j < 9; ++j) {
+        if (board[i][j] != '.') {
+          int num = board[i][j] - '1';
+          if (row_flag[i][num]) {
+            return false;
+          } else {
+            row_flag[i][num] = true;
+          }
+          if (col_flag[num][j]) {
+            return false;
+          } else {
+            col_flag[num][j] = true;
+          }
+          int square_num = GetSquareNumber(i, j);
+          if (square_flag[square_num][num]) {
+            return false;
+          } else {
+            square_flag[square_num][num] = true;
+          }
+        }
+      }
     }
+    return true;
+  }
+ private:
+  int GetSquareNumber(int m, int n) {
+    if (n < 3) {
+      // 0 ~ 2
+      if (m < 3) {
+        return 0;
+      } else if (m < 6) {
+        return 1;
+      } else {
+        return 2;
+      }
+    } else if (n < 6) {
+      // 3 ~ 5
+      if (m < 3) {
+        return 3;
+      } else if (m < 6) {
+        return 4;
+      } else {
+        return 5;
+      }
+    } else {
+      // 6 ~ 8
+      if (m < 3) {
+        return 6;
+      } else if (m < 6) {
+        return 7;
+      } else {
+        return 8;
+      }
+    }
+  }
 };
+
 // @lc code=end
 
 int main(int argc, char* argv[]) {
-
+  Solution solution;
+  std::cout << "do nothing for unit test" << std::endl;
   return 0;
 }
