77ffb014982896c5c8f497885b8ba306ff068382
lucasktian <lucasktian@tencent.com>
2023-06-15 23:17:15 +0800

    [UPD]1. upload 47 problem; 2. problem 47 solution has some bad case;

--- cpp_implement/47.permutations-ii.cpp
+++ cpp_implement/47.permutations-ii.cpp
@@ -1,74 +1,79 @@
 /*
- * @lc app=leetcode.cn id=46 lang=cpp
+ * @lc app=leetcode.cn id=47 lang=cpp
  *
- * [46] Permutations
+ * [47] Permutations II
  *
- * https://leetcode.cn/problems/permutations/description/
+ * https://leetcode.cn/problems/permutations-ii/description/
  *
  * algorithms
- * Medium (78.88%)
- * Likes:    2577
+ * Medium (65.53%)
+ * Likes:    1383
  * Dislikes: 0
- * Total Accepted:    875.3K
- * Total Submissions: 1.1M
- * Testcase Example:  '[1,2,3]'
+ * Total Accepted:    461.9K
+ * Total Submissions: 704.9K
+ * Testcase Example:  '[1,1,2]'
  *
- * Given an array nums of distinct integers, return all the possible
- * permutations. You can return the answer in any order.
+ * Given a collection of numbers, nums, that might contain duplicates, return
+ * all possible unique permutations in any order.
+ *
  *
  * Example 1:
+ *
+ *
+ * Input: nums = [1,1,2]
+ * Output:
+ * [[1,1,2],
+ * ⁠[1,2,1],
+ * ⁠[2,1,1]]
+ *
+ *
+ * Example 2:
+ *
+ *
  * Input: nums = [1,2,3]
  * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
- * Example 2:
- * Input: nums = [0,1]
- * Output: [[0,1],[1,0]]
- * Example 3:
- * Input: nums = [1]
- * Output: [[1]]
+ *
+ *
  *
  * Constraints:
  *
- * 1 <= nums.length <= 6
+ *
+ * 1 <= nums.length <= 8
  * -10 <= nums[i] <= 10
- * All the integers of nums are unique.
+ *
+ *
  */
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include "integer_utils.h"
 // @lc code=start
-/**
- * 我的编程思路应该是:先有思路,然后将思路通过代码实现出来
- * 这道题的时间复杂度:O(n!), 空间复杂度: O(n^2),由于是要将所有的组合都列出来,所以时间和空间复杂度都不能再优化了.
- *
- * nums: [1, 2, 3, 4]
- * ans:  [1, 2, 3, 4] [1, 2, 4, 3] [1, 3, 2, 4] [1, 3, 4, 2] [1, 4, 2, 3] [1, 4, 3, 2]
- *
- * */
 class Solution {
  public:
-  std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+  std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums) {
     std::vector<std::vector<int>> ans;
-    auto N = nums.size();
-    std::vector<bool> flags(N, false);
     std::vector<int> permutation;
-    BackTrack(nums, flags, permutation, ans);
+    std::sort(nums.begin(), nums.end());
+    std::vector<bool> flags(nums.size(), false);
+    BackTrack(nums, 0, flags, permutation, ans);
     return ans;
   }
 
  private:
-  void BackTrack(std::vector<int>& nums, std::vector<bool>& flags, std::vector<int>& permutation,
+  void BackTrack(std::vector<int>& nums, int index, std::vector<bool>& flags, std::vector<int>& permutation,
                  std::vector<std::vector<int>>& ans) {
     if (permutation.size() == nums.size()) {
       ans.push_back(permutation);
       return;
     }
     for (int i = 0; i < nums.size(); ++i) {
+      if ((i > index) && nums[i - 1] == nums[i]) {
+        continue;
+      }
       if (!flags[i]) {
         flags[i] = true;
         permutation.push_back(nums[i]);
-        BackTrack(nums, flags, permutation, ans);
+        BackTrack(nums, i + 1, flags, permutation, ans);
         permutation.pop_back();
         flags[i] = false;
       }
@@ -80,12 +85,12 @@ class Solution {
 int main(int argc, char* argv[]) {
   Solution solution;
   std::vector<std::string> test_test;
+  test_test.push_back("[1,1,2]");
   test_test.push_back("[1,2,3]");
-  test_test.push_back("[0,1]");
   for (int i = 0; i < test_test.size(); ++i) {
     std::cout << test_test[i] << " permute: ";
     auto nums = StringToIntegerVector(test_test[i]);
-    auto ans  = solution.permute(nums);
+    auto ans  = solution.permuteUnique(nums);
     for (int j = 0; j < ans.size(); ++j) {
       std::string message = "[";
       for (const auto& iter : ans[j]) {
