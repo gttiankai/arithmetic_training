899df570a3ad7f9751333ff8e28f7cda03b6865e
lucasktian <lucasktian@tencent.com>
2023-07-23 20:23:18 +0800

    [UPD]1. hit 49 problem;

--- cpp_implement/49.group-anagrams.cpp
+++ cpp_implement/49.group-anagrams.cpp
@@ -38,13 +38,27 @@
  */
 
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 // @lc code=start
 class Solution {
  public:
-  std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {}
+  std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+    std::map<std::string, std::vector<std::string>> unorder_map;
+    for (const auto& item : strs) {
+      std::string key = item;
+      std::sort(key.begin(), key.end());
+      // 使用排序后的字符作为 key 是比较好的思路
+      unorder_map[key].push_back(item);
+    }
+    std::vector<std::vector<std::string>> ans;
+    for (const auto& item : unorder_map) {
+      ans.push_back(item.second);
+    }
+    return ans;
+  }
 };
 // @lc code=end
 
@@ -55,7 +69,7 @@ int main(int argc, char* argv[]) {
   for (const auto& item : ans) {
     std::string message = "";
     for (const auto& str : item) {
-      message += ", " + str;
+      message += str + ", ";
     }
     std::cout << message << std::endl;
   }
