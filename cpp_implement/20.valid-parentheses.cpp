00cd2c6abc34141700eceb62320e27c494b79c2c
lucasktian <lucasktian@tencent.com>
2025-02-14 12:33:24 +0800

    [UPD]1. hit 20 problem;

--- cpp_implement/20.valid-parentheses.cpp
+++ cpp_implement/20.valid-parentheses.cpp
@@ -2,55 +2,89 @@
  * @lc app=leetcode.cn id=20 lang=cpp
  *
  * [20] Valid Parentheses
+ *
+ * https://leetcode.cn/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (44.50%)
+ * Likes:    4631
+ * Dislikes: 0
+ * Total Accepted:    2.1M
+ * Total Submissions: 4.6M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ *
+ * Example 1:
+ * Input: s = "()"
+ * Output: true
+ *
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ *
+ * Example 3:
+ * Input: s = "(]"
+ * Output: false
+ *
+ * Example 4:
+ * Input: s = "([])"
+ * Output: true
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 10^4
+ * s consists of parentheses only '()[]{}'.
  */
-
-// @lc code=start
-
 #include <iostream>
 #include <stack>
 #include <string>
-
-/**
- * 使用 stack 一次性通过
- *
- * */
+// @lc code=start
 class Solution {
- public:
-  bool isValid(std::string s) {
-    std::stack<char> stack;
-    for (int i = 0; i < s.size(); ++i) {
-      if (stack.empty()) {
-        stack.push(s[i]);
-        continue;
-      }
-
-      if (Match(stack.top(), s[i])) {
-        stack.pop();
-      } else {
-        stack.push(s[i]);
-      }
+   public:
+    bool isValid(std::string s) {
+        std::stack<char> brackets_stack;
+        for (char& item : s) {
+            if (brackets_stack.empty()) {
+                brackets_stack.push(item);
+                continue;
+            }
+            if (match(brackets_stack.top(), item)) {
+                brackets_stack.pop();
+            } else {
+                brackets_stack.push(item);
+            }
+        }
+        return brackets_stack.empty();
     }
-    return stack.empty();
-  }
 
- private:
-  bool Match(char left, char right) {
-    if (left == '(') {
-      return right == ')';
-    } else if (left == '{') {
-      return right == '}';
-    } else if (left == '[') {
-      return right == ']';
-    } else {
-      return false;
+   private:
+    bool match(char left, char right) {
+        if (left == '[' && right == ']') {
+            return true;
+        } else if (left == '(' && right == ')') {
+            return true;
+        } else if (left == '{' && right == '}') {
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
 };
+// @lc code=end
 
 int main(int argc, char* argv[]) {
-  std::string message = "{()[]{}]";
-  Solution solution;
-  std::cout << solution.isValid(message) << std::endl;
-  return 0;
+    std::string message = "{()[]{}]";
+    Solution solution;
+    std::cout << solution.isValid(message) << std::endl;
+    return 0;
 }
-// @lc code=end
