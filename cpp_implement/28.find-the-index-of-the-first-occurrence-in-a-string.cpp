42c87520207d3aaf9d707a2188ebc9d7fe8e54b9
lucasktian <lucasktian@tencent.com>
2025-02-17 21:27:27 +0800

    [UPD]1. hit 28 problem;

--- cpp_implement/28.find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ cpp_implement/28.find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,11 +6,11 @@
  * https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
  *
  * algorithms
- * Medium (42.39%)
- * Likes:    1874
+ * Easy (44.55%)
+ * Likes:    2360
  * Dislikes: 0
- * Total Accepted:    849.5K
- * Total Submissions: 2M
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.7M
  * Testcase Example:  '"sadbutsad"\n"sad"'
  *
  * Given two strings needle and haystack, return the index of the first
@@ -19,8 +19,6 @@
  *
  *
  * Example 1:
- *
- *
  * Input: haystack = "sadbutsad", needle = "sad"
  * Output: 0
  * Explanation: "sad" occurs at index 0 and 6.
@@ -28,69 +26,52 @@
  *
  *
  * Example 2:
- *
- *
  * Input: haystack = "leetcode", needle = "leeto"
  * Output: -1
  * Explanation: "leeto" did not occur in "leetcode", so we return -1.
  *
- *
- *
  * Constraints:
- *
  *
  * 1 <= haystack.length, needle.length <= 10^4
  * haystack and needle consist of only lowercase English characters.
  *
- *
  */
 #include <iostream>
-#include <ostream>
 #include <string>
 #include <vector>
-
 // @lc code=start
 class Solution {
- public:
-  int strStr(std::string haystack, std::string needle) {
-    int M = haystack.size();
-    int N = needle.size();
-    if (M < N) {
-      return -1;
+   public:
+    int strStr(std::string haystack, std::string needle) {
+        int index = -1;
+        if (needle.size() > haystack.size()) {
+            return index;
+        }
+        int n = needle.size();
+        for (int i = 0; i < haystack.size() - n + 1; ++i) {
+            if (needle == haystack.substr(i, n)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
-    for (int i = 0; i <= M - N; ++i) {
-      if (Equal(haystack, needle, i)) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
- private:
-  bool Equal(std::string& haystack, std::string& needle, int start) {
-    int N = needle.size();
-    for (int j = 0; j < N; ++j) {
-      if (haystack[start + j] != needle[j]) {
-        return false;
-      }
-    }
-    return true;
-  }
 };
 // @lc code=end
 
 int main(int argc, char* argv[]) {
-  Solution solution;
-  std::vector<std::string> haystackes;
-  std::vector<std::string> needles;
-  haystackes.push_back("sadbutsad");
-  needles.push_back("sad");
-  haystackes.push_back("leetcode");
-  needles.push_back("leeto");
-  haystackes.push_back("a");
-  needles.push_back("a");
-  for (int i = 0; i < haystackes.size(); ++i) {
-    std::cout << haystackes[i] << " " << needles[i] << " " << solution.strStr(haystackes[i], needles[i]) << std::endl;
-  }
-  return 0;
+    Solution solution;
+    std::vector<std::string> haystackes;
+    std::vector<std::string> needles;
+    haystackes.push_back("sadbutsad");
+    needles.push_back("sad");
+    haystackes.push_back("leetcode");
+    needles.push_back("leeto");
+    haystackes.push_back("a");
+    needles.push_back("a");
+    for (int i = 0; i < haystackes.size(); ++i) {
+        std::cout << haystackes[i] << " " << needles[i] << " " << solution.strStr(haystackes[i], needles[i])
+                  << std::endl;
+    }
+    return 0;
 }
