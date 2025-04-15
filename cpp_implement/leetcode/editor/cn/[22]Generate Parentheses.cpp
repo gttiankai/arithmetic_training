dc2647c228567b0f34eb44498bb51d2cfd7a15d8
lucasktian <lucasktian@tencent.com>
2025-04-15 23:34:40 +0800

    [UPD]1. upload 22 problem;

---
+++ cpp_implement/leetcode/editor/cn/[22]Generate Parentheses.cpp
@@ -0,0 +1,35 @@
+/**
+Given n pairs of parentheses, write a function to generate all combinations of
+well-formed parentheses.
+
+
+ Example 1:
+ Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+ Example 2:
+ Input: n = 1
+Output: ["()"]
+
+
+ Constraints:
+
+
+ 1 <= n <= 8
+
+
+ Related Topics 字符串 动态规划 回溯 👍 3819 👎 0
+
+*/
+#include <string>
+#include <vector>
+// leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+   public:
+    std::vector<std::string> generateParenthesis(int n) {
+        
+
+
+    }
+};
+// leetcode submit region end(Prohibit modification and deletion)
