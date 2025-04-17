1f38a1044b38e4824a8bdc768a8230c3d16574fd
gttiankai <gttiankai@163.com>
2025-04-17 21:42:43 +0800

    [UPD]1. hit 22 problem;

--- cpp_implement/leetcode/editor/cn/[22]Generate Parentheses.cpp
+++ cpp_implement/leetcode/editor/cn/[22]Generate Parentheses.cpp
@@ -21,15 +21,39 @@ Output: ["()"]
  Related Topics 字符串 动态规划 回溯 👍 3819 👎 0
 
 */
+#include <ios>
 #include <string>
 #include <vector>
 // leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
    public:
     std::vector<std::string> generateParenthesis(int n) {
-        
+        std::vector<std::string> ans;
+        std::string parenthesis;
+        BackTracking(ans, parenthesis, 0, 0, n);
+        return ans;
+    }
 
-
+   private:
+    void BackTracking(std::vector<std::string>& ans, std::string parenthesis,
+                      int left, int right, int n) {
+        if (right > left) {
+            return;
+        }
+        if (left == n && left == right) {
+            ans.push_back(parenthesis);
+            return;
+        }
+        if (left < n) {
+            parenthesis.push_back('(');
+            BackTracking(ans, parenthesis, left + 1, right, n);
+            parenthesis.pop_back();
+        }
+        if (left > right) {
+            parenthesis.push_back(')');
+            BackTracking(ans, parenthesis, left, right + 1, n);
+            parenthesis.pop_back();
+        }
     }
 };
 // leetcode submit region end(Prohibit modification and deletion)
