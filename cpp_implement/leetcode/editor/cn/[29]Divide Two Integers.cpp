68af9e88717a701b80bfa9c23eeaab151135ea42
gttiankai <gttiankai@163.com>
2025-04-18 16:55:08 +0800

    [UPD]1. implemented a relatively slow solution for 29 problem;

--- cpp_implement/leetcode/editor/cn/[29]Divide Two Integers.cpp
+++ cpp_implement/leetcode/editor/cn/[29]Divide Two Integers.cpp
@@ -39,20 +39,53 @@
 //
 //    Related Topics 位运算 数学 👍 1281 👎 0
 
-#include <__ostream/basic_ostream.h>
-
+#include <climits>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 // leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
    public:
-    int divide(int dividend, int divisor) {}
+    // 仔细的思考题目,你会发现由于是正数除法,发生溢出的情况比较少见,目前只有一种
+    // 情况会发生溢出: INT_MIN / -1,出现这种情况下才会发生溢出
+    int divide(int dividend, int divisor) {
+        bool same_sign = CheckSign(dividend, divisor);
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        int ans = 0;
+        if (dividend > 0) {
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+        }
+        while (dividend <= divisor) {
+            // 变成减法的原因是 INT 的负数值域比正数的值域大一个,用于处理 INT_MIN 的情况
+            ans--;
+            dividend -= divisor;
+        }
+        if (same_sign) {
+            // 由于上面的 ans 是减法,所以符号相同时,需要添加负号
+            return -ans;
+        }
+        return ans;
+    }
+
+   private:
+    bool CheckSign(int dividend, int divisor) {
+        if ((dividend >= 0 and divisor > 0) || (dividend <= 0 && divisor < 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 // leetcode submit region end(Prohibit modification and deletion)
 int main(int argc, char *argv[]) {
     Solution solution;
-    std::vector<int> dividend_array = {10, 10, -10};
-    std::vector<int> divisor_array  = {3, -3, 3};
+    std::vector<int> dividend_array = {10, 10, -10, INT_MIN, INT_MIN, INT_MAX};
+    std::vector<int> divisor_array  = {3, -3, 3, -1, 1, -1};
     for (int i = 0; i < dividend_array.size(); ++i) {
         int dividend = dividend_array[i];
         int divisor  = divisor_array[i];
