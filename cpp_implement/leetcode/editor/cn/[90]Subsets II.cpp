c7276e1a9465c4ed544ec934920cad070d317239
lucasktian <lucasktian@tencent.com>
2025-05-11 23:23:27 +0800

    [UPD]1. upload 90 problem;

---
+++ cpp_implement/leetcode/editor/cn/[90]Subsets II.cpp
@@ -0,0 +1,34 @@
+//Given an integer array nums that may contain duplicates, return all possible 
+//subsets (the power set). 
+//
+// The solution set must not contain duplicate subsets. Return the solution in 
+//any order. 
+//
+// 
+// Example 1: 
+// Input: nums = [1,2,2]
+//Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+// 
+// Example 2: 
+// Input: nums = [0]
+//Output: [[],[0]]
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10 
+// -10 <= nums[i] <= 10 
+// 
+//
+// Related Topics 位运算 数组 回溯 👍 1307 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
