0597013eafcf9c515178e8d1bcba5b10e9f5bc6a
lucasktian <lucasktian@tencent.com>
2025-05-13 00:18:35 +0800

    [UPD]1. upload 91 problem;

--- cpp_implement/leetcode/editor/cn/[91]Decode Ways.cpp
+++ cpp_implement/leetcode/editor/cn/[91]Decode Ways.cpp
@@ -1,81 +1,89 @@
-//You have intercepted a secret message encoded as a string of numbers. The 
-//message is decoded via the following mapping: 
+// You have intercepted a secret message encoded as a string of numbers. The
+// message is decoded via the following mapping:
 //
-// "1" -> 'A' "2" -> 'B' ... "25" -> 'Y' "26" -> 'Z' 
+//  "1" -> 'A' "2" -> 'B' ... "25" -> 'Y' "26" -> 'Z'
 //
-// However, while decoding the message, you realize that there are many 
-//different ways you can decode the message because some codes are contained in other 
-//codes ("2" and "5" vs "25"). 
+//  However, while decoding the message, you realize that there are many
+// different ways you can decode the message because some codes are contained in
+// other codes ("2" and "5" vs "25").
 //
-// For example, "11106" can be decoded into: 
+//  For example, "11106" can be decoded into:
 //
-// 
-// "AAJF" with the grouping (1, 1, 10, 6) 
-// "KJF" with the grouping (11, 10, 6) 
-// The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6
-//" is valid). 
-// 
 //
-// Note: there may be strings that are impossible to decode. Given a string s 
-//containing only digits, return the number of ways to decode it. If the entire 
-//string cannot be decoded in any valid way, return 0. 
+//  "AAJF" with the grouping (1, 1, 10, 6)
+//  "KJF" with the grouping (11, 10, 6)
+//  The grouping (1, 11, 06) is invalid because "06" is not a valid code (only
+//  "6
+//" is valid).
 //
-// The test cases are generated so that the answer fits in a 32-bit integer. 
 //
-// 
-// Example 1: 
+//  Note: there may be strings that are impossible to decode. Given a string s
+// containing only digits, return the number of ways to decode it. If the entire
+// string cannot be decoded in any valid way, return 0.
 //
-// 
-// Input: s = "12" 
-// 
+//  The test cases are generated so that the answer fits in a 32-bit integer.
 //
-// Output: 2 
 //
-// Explanation: 
+//  Example 1:
 //
-// "12" could be decoded as "AB" (1 2) or "L" (12). 
 //
-// Example 2: 
+//  Input: s = "12"
 //
-// 
-// Input: s = "226" 
-// 
 //
-// Output: 3 
+//  Output: 2
 //
-// Explanation: 
+//  Explanation:
 //
-// "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6). 
+//  "12" could be decoded as "AB" (1 2) or "L" (12).
 //
-// Example 3: 
+//  Example 2:
 //
-// 
-// Input: s = "06" 
-// 
 //
-// Output: 0 
+//  Input: s = "226"
 //
-// Explanation: 
 //
-// "06" cannot be mapped to "F" because of the leading zero ("6" is different 
-//from "06"). In this case, the string is not a valid encoding, so return 0. 
+//  Output: 3
 //
-// 
-// Constraints: 
+//  Explanation:
 //
-// 
-// 1 <= s.length <= 100 
-// s contains only digits and may contain leading zero(s). 
-// 
+//  "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 //
-// Related Topics 字符串 动态规划 👍 1602 👎 0
+//  Example 3:
+//
+//
+//  Input: s = "06"
+//
+//
+//  Output: 0
+//
+//  Explanation:
+//
+//  "06" cannot be mapped to "F" because of the leading zero ("6" is different
+// from "06"). In this case, the string is not a valid encoding, so return 0.
+//
+//
+//  Constraints:
+//
+//
+//  1 <= s.length <= 100
+//  s contains only digits and may contain leading zero(s).
+//
+//
+//  Related Topics 字符串 动态规划 👍 1602 👎 0
+#include <__ostream/basic_ostream.h>
 
+#include <iostream>
+#include <vector>
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-public:
-    int numDecodings(string s) {
-        
-    }
+   public:
+    int numDecodings(std::string s) {}
 };
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
+
+int main(int argc, char *argv[]) {
+    std::string s = "12";
+    Solution solution;
+    std::cout << solution.numDecodings(s) << std::endl;
+}
