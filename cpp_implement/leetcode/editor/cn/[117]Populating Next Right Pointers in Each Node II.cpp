0aa5d8114c35e16d1888b1f3fbf2de52ab90ef49
lucasktian <lucasktian@tencent.com>
2025-05-28 23:18:59 +0800

    [UPD]1. 117 题的实现比较复杂,需要一个比较简单的解法;

--- cpp_implement/leetcode/editor/cn/[117]Populating Next Right Pointers in Each Node II.cpp
+++ cpp_implement/leetcode/editor/cn/[117]Populating Next Right Pointers in Each Node II.cpp
@@ -1,60 +1,65 @@
-//Given a binary tree 
+// Given a binary tree
 //
-// 
-//struct Node {
-//  int val;
-//  Node *left;
-//  Node *right;
-//  Node *next;
-//}
-// 
 //
-// Populate each next pointer to point to its next right node. If there is no 
-//next right node, the next pointer should be set to NULL. 
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
 //
-// Initially, all next pointers are set to NULL. 
 //
-// 
-// Example 1: 
-// 
-// 
-//Input: root = [1,2,3,4,5,null,7]
-//Output: [1,#,2,3,#,4,5,7,#]
-//Explanation: Given the above binary tree (Figure A), your function should 
-//populate each next pointer to point to its next right node, just like in Figure B. 
-//The serialized output is in level order as connected by the next pointers, with 
+//  Populate each next pointer to point to its next right node. If there is no
+// next right node, the next pointer should be set to NULL.
+//
+//  Initially, all next pointers are set to NULL.
+//
+//
+//  Example 1:
+//
+//
+// Input: root = [1,2,3,4,5,null,7]
+// Output: [1,#,2,3,#,4,5,7,#]
+// Explanation: Given the above binary tree (Figure A), your function should
+// populate each next pointer to point to its next right node, just like in
+// Figure B. The serialized output is in level order as connected by the next
+// pointers, with
 //'#' signifying the end of each level.
-// 
 //
-// Example 2: 
 //
-// 
-//Input: root = []
-//Output: []
-// 
+//  Example 2:
 //
-// 
-// Constraints: 
 //
-// 
-// The number of nodes in the tree is in the range [0, 6000]. 
-// -100 <= Node.val <= 100 
-// 
+// Input: root = []
+// Output: []
 //
-// 
-// Follow-up: 
 //
-// 
-// You may only use constant extra space. 
-// The recursive approach is fine. You may assume implicit stack space does not 
-//count as extra space for this problem. 
-// 
 //
-// Related Topics 树 深度优先搜索 广度优先搜索 链表 二叉树 👍 915 👎 0
+//  Constraints:
+//
+//
+//  The number of nodes in the tree is in the range [0, 6000].
+//  -100 <= Node.val <= 100
+//
+//
+//
+//  Follow-up:
+//
+//
+//  You may only use constant extra space.
+//  The recursive approach is fine. You may assume implicit stack space does not
+// count as extra space for this problem.
+//
+//
+//  Related Topics 树 深度优先搜索 广度优先搜索 链表 二叉树 👍 915 👎 0
+
+#include <sys/syslimits.h>
+
+#include <algorithm>
 
 #include "node_utils.h"
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 /*
 // Definition for a Node.
 class Node {
@@ -74,15 +79,72 @@ public:
 */
 
 class Solution {
-public:
+   public:
     /**
      * 使用层次遍历是可以轻松的解决的, 但是有没有时间复杂度是 O(1) 的解法呢?
      *
      ***/
     Node* connect(Node* root) {
         if (root == nullptr) return root;
-
-
+        Node* cur = root;
+        while (cur != nullptr) {
+            Node* first_node = cur;
+            for (Node* p = cur; p != nullptr; p = p->next) {
+                if (p->left != nullptr) {
+                    if (p->right != nullptr) {
+                        p->left->next = p->right;
+                    } else {
+                        Node* q = p;
+                        while (q->next != nullptr) {
+                            if (q->next->left != nullptr) {
+                                p->left->next = q->next->left;
+                                break;
+                            }
+                            if (q->next->right != nullptr) {
+                                p->left->next = q->next->right;
+                                break;
+                            }
+                            q = q->next;
+                        }
+                    }
+                }
+                if (p->right != nullptr) {
+                    Node* q = p;
+                    while (q->next != nullptr) {
+                        if (q->next->left != nullptr) {
+                            p->right->next = q->next->left;
+                            break;
+                        }
+                        if (q->next->right != nullptr) {
+                            p->right->next = q->next->right;
+                            break;
+                        }
+                        q = q->next;
+                    }
+                }
+            }
+            while (first_node != nullptr) {
+                if (first_node->left != nullptr) {
+                    cur = first_node->left;
+                    break;
+                }
+                if (first_node->right != nullptr) {
+                    cur = first_node->right;
+                    break;
+                }
+                first_node = first_node->next;
+                cur        = first_node;
+            }
+        }
+        return root;
     }
 };
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
+
+int main(int argc, char* argv[]) {
+    Solution solution;
+    // Node* root = StringToNode("[1,2,3,4,5,null,7]");
+    // Node* root = StringToNode("[1,2,3,4,5,null,6,7,null,null,null,null,8]");
+    Node* root = StringToNode("[1,2,3,4,5]");
+    solution.connect(root);
+}
--- cpp_implement/node_utils.h
+++ cpp_implement/node_utils.h
@@ -17,17 +17,103 @@
 
 #pragma once
 
-class Node {
-   public:
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+struct Node {
     int val;
-    Node* left;
-    Node* right;
-    Node* next;
+    Node *left;
+    Node *right;
+    Node *next;
 
     Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
 
     Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
 
-    Node(int _val, Node* _left, Node* _right, Node* _next)
+    Node(int _val, Node *_left, Node *_right, Node *_next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
+
+void TrimLeftTrailingSpaces(std::string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(),
+                                       [](int ch) { return !isspace(ch); }));
+}
+
+void TrimRightTrailingSpaces(std::string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(),
+                        [](int ch) { return !isspace(ch); })
+                    .base(),
+                input.end());
+}
+
+Node *StringToNode(std::string input) {
+    TrimLeftTrailingSpaces(input);
+    TrimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    std::string item;
+    std::stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    Node *root = new Node(stoi(item));
+    std::queue<Node *> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        Node *node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        TrimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left     = new Node(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        TrimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right     = new Node(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+std::string NodeToString(Node *root) {
+    if (root == nullptr) {
+        return "[]";
+    }
+
+    std::string output = "";
+    std::queue<Node *> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node *node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+            output += "null, ";
+            continue;
+        }
+
+        output += std::to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
