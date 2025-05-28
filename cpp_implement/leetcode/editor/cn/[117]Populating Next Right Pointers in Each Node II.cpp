5014f65c312f25be2c13a4083545fb067fc0e2b0
lucasktian <lucasktian@tencent.com>
2025-05-28 23:42:08 +0800

    [UPD]1. 上传了 117 比较容易理解的解法;

--- cpp_implement/leetcode/editor/cn/[117]Populating Next Right Pointers in Each Node II.cpp
+++ cpp_implement/leetcode/editor/cn/[117]Populating Next Right Pointers in Each Node II.cpp
@@ -82,9 +82,14 @@ class Solution {
    public:
     /**
      * 使用层次遍历是可以轻松的解决的, 但是有没有时间复杂度是 O(1) 的解法呢?
-     *
+     * 由于需要遍历每个节点,所以不存在时间复杂度为 O(1) 的解法,
+     * 这个题目的本质是考察是否有空间复杂度为 O(1) 的解法?
+     * 可以利用的点是:
+     *   1. 如果想要处理第 i 层的节点,i - 1 层的节点的 next 指针已经处理好,所以
+     *      第 i-1 层的节点会通过 next 指针形成链表.
+     *   2. 第 0 层由于只有一个节点,默认已经处理好了
      ***/
-    Node* connect(Node* root) {
+    Node* connectComple(Node* root) {
         if (root == nullptr) return root;
         Node* cur = root;
         while (cur != nullptr) {
@@ -137,6 +142,59 @@ class Solution {
             }
         }
         return root;
+    }
+
+   public:
+    Node* connect(Node* root) {
+        if (root == nullptr) return root;
+        Node* start = root;
+        while (start != nullptr) {
+            Node* next_start = nullptr;
+            Node* last       = nullptr;
+            for (Node* p = start; p != nullptr; p = p->next) {
+                if (p->left != nullptr) {
+                    // handle(last, p->left, next_start);
+                    // 下面是将 handle 中的实现在主函数中
+                    if (last != nullptr) {
+                        last->next = p->left;
+                    }
+                    if (next_start == nullptr) {
+                        next_start = p->left;
+                    }
+                    last = p->left;
+                }
+                if (p->right != nullptr) {
+                    // handle(last, p->right, next_start);
+                    if (last != nullptr) {
+                        last->next = p->right;
+                    }
+                    if (next_start == nullptr) {
+                        next_start = p->right;
+                    }
+                    last = p->right;
+                }
+            }
+            start = next_start;
+        }
+        return root;
+    }
+
+   private:
+    /**
+     *  下面的实现好精妙啊
+     *  last: 代表当前层次遍历中,上一个处理过的 node
+     *  next_start: 代表是下一层的第一个 node
+     *  p: 代表当前正在处理的 node
+     **/
+    void handle(Node*& last, Node*& p, Node*& next_start) {
+        if (last != nullptr) {
+            last->next = p;
+        }
+        // last == nullptr
+        if (next_start == nullptr) {
+            next_start = p;
+        }
+        last = p;
     }
 };
 // leetcode submit region end(Prohibit modification and deletion)
--- cpp_implement/tree_node.h
+++ cpp_implement/tree_node.h
@@ -1,16 +1,19 @@
-// Tencent is pleased to support the open source community by making TNN available.
+// Tencent is pleased to support the open source community by making TNN
+// available.
 //
 // Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
 //
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of the
+// License at
 //
 // https://opensource.org/licenses/BSD-3-Clause
 //
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
 
 #ifndef ARITHMETIC_TRAINING_TREE_NODE_H
 #define ARITHMETIC_TRAINING_TREE_NODE_H
@@ -19,90 +22,95 @@
 #include <sstream>
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 
 void TrimLeftTrailingSpaces(std::string &input) {
-  input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) { return !isspace(ch); }));
+    input.erase(input.begin(), find_if(input.begin(), input.end(),
+                                       [](int ch) { return !isspace(ch); }));
 }
 
 void TrimRightTrailingSpaces(std::string &input) {
-  input.erase(find_if(input.rbegin(), input.rend(), [](int ch) { return !isspace(ch); }).base(), input.end());
+    input.erase(find_if(input.rbegin(), input.rend(),
+                        [](int ch) { return !isspace(ch); })
+                    .base(),
+                input.end());
 }
 
 TreeNode *StringToTreeNode(std::string input) {
-  TrimLeftTrailingSpaces(input);
-  TrimRightTrailingSpaces(input);
-  input = input.substr(1, input.length() - 2);
-  if (!input.size()) {
-    return nullptr;
-  }
-
-  std::string item;
-  std::stringstream ss;
-  ss.str(input);
-
-  getline(ss, item, ',');
-  TreeNode *root = new TreeNode(stoi(item));
-  std::queue<TreeNode *> nodeQueue;
-  nodeQueue.push(root);
-
-  while (true) {
-    TreeNode *node = nodeQueue.front();
-    nodeQueue.pop();
-
-    if (!getline(ss, item, ',')) {
-      break;
+    TrimLeftTrailingSpaces(input);
+    TrimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
     }
 
-    TrimLeftTrailingSpaces(item);
-    if (item != "null") {
-      int leftNumber = stoi(item);
-      node->left     = new TreeNode(leftNumber);
-      nodeQueue.push(node->left);
-    }
+    std::string item;
+    std::stringstream ss;
+    ss.str(input);
 
-    if (!getline(ss, item, ',')) {
-      break;
-    }
+    getline(ss, item, ',');
+    TreeNode *root = new TreeNode(stoi(item));
+    std::queue<TreeNode *> nodeQueue;
+    nodeQueue.push(root);
 
-    TrimLeftTrailingSpaces(item);
-    if (item != "null") {
-      int rightNumber = stoi(item);
-      node->right     = new TreeNode(rightNumber);
-      nodeQueue.push(node->right);
+    while (true) {
+        TreeNode *node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        TrimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left     = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        TrimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right     = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
     }
-  }
-  return root;
+    return root;
 }
 
 std::string TreeNodeToString(TreeNode *root) {
-  if (root == nullptr) {
-    return "[]";
-  }
-
-  std::string output = "";
-  std::queue<TreeNode *> q;
-  q.push(root);
-  while (!q.empty()) {
-    TreeNode *node = q.front();
-    q.pop();
-
-    if (node == nullptr) {
-      output += "null, ";
-      continue;
+    if (root == nullptr) {
+        return "[]";
     }
 
-    output += std::to_string(node->val) + ", ";
-    q.push(node->left);
-    q.push(node->right);
-  }
-  return "[" + output.substr(0, output.length() - 2) + "]";
+    std::string output = "";
+    std::queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+        TreeNode *node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+            output += "null, ";
+            continue;
+        }
+
+        output += std::to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
 }
 
 #endif  // ARITHMETIC_TRAINING_TREE_NODE_H
