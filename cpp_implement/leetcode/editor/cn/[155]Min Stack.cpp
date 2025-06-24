ebe3f55869a39e78c80f849c517048ab148fca22
lucasktian <lucasktian@tencent.com>
2025-06-24 20:44:20 +0800

    [UPD]1. hit 155 problem;

---
+++ cpp_implement/leetcode/editor/cn/[155]Min Stack.cpp
@@ -0,0 +1,83 @@
+//Design a stack that supports push, pop, top, and retrieving the minimum 
+//element in constant time. 
+//
+// Implement the MinStack class: 
+//
+// 
+// MinStack() initializes the stack object. 
+// void push(int val) pushes the element val onto the stack. 
+// void pop() removes the element on the top of the stack. 
+// int top() gets the top element of the stack. 
+// int getMin() retrieves the minimum element in the stack. 
+// 
+//
+// You must implement a solution with O(1) time complexity for each function. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+//[[],[-2],[0],[-3],[],[],[],[]]
+//
+//Output
+//[null,null,null,null,-3,null,0,-2]
+//
+//Explanation
+//MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin(); // return -3
+//minStack.pop();
+//minStack.top();    // return 0
+//minStack.getMin(); // return -2
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -2³¹ <= val <= 2³¹ - 1 
+// Methods pop, top and getMin operations will always be called on non-empty 
+//stacks. 
+// At most 3 * 10⁴ calls will be made to push, pop, top, and getMin. 
+// 
+//
+//  Related Topics 栈 设计 👍 1935 👎 0
+#include <vector>
+// leetcode submit region begin(Prohibit modification and deletion)
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        
+    }
+    
+    void pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    int getMin() {
+        
+    }
+    private:
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
